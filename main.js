1c1e478cbf72c9137c5d4c303e132ec56833633c
Bogdan Kowaltchook <bogdan.kowaltchook@codeimmersives.com>
2020-02-25 14:37:23 -0500

    8 passed, 8 total

--- main.js
+++ main.js
@@ -1,7 +1,45 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+function onlyOdds (numbers) {
+  const numbersOdds = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 1) {
+      numbersOdds.push(numbers[i]);
+    }
+  }
+  return numbersOdds;
+}
 
+function onlyEvens (numbers) {
+  const numbersEven = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+      numbersEven.push(numbers[i]);
+    }
+  }
+  return numbersEven;
+}
+
+function shortNamesOnly (names) {
+  const namesShort = [];
+  for (let i = 0; i < names.length; i++) {
+    if (names[i].length < 7) {
+      namesShort.push(names[i]);
+    }
+  }
+  return namesShort;
+}
+
+function dNames (names) {
+  const namesThatStartWithD = [];
+  for (let i = 0; i < names.length; i++) {
+    if (names[i][0] === 'D' || names[i][0] === 'd') {
+      namesThatStartWithD.push(names[i]);
+    }
+  }
+  return namesThatStartWithD;
+}
 
 
 
